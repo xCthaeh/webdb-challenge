23cb450b7bf80b38ee17e07cba9d0322c076c6f0
Alyssa Jane <xcthaeh@gmail.com>
2019-06-07 12:06:22 -0400

    added an actions seed

---
+++ data/seeds/02_actions.js
@@ -0,0 +1,42 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("actions")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("actions").insert([
+        {
+          description: "Action 1 for Sprint 1",
+          notes: "notes",
+          completed: true,
+          project_id: 1
+        },
+        {
+          description: "Action 2 Sprint 1",
+          notes: "notes",
+          completed: false,
+          project_id: 1
+        },
+
+        {
+          description: "Action 1 Sprint 2",
+          notes: "notes",
+          completed: true,
+          project_id: 2
+        },
+        {
+          description: "Action 2 for Sprint 2",
+          notes: "notes",
+          completed: true,
+          project_id: 2
+        },
+
+        {
+          description: "Action 1 for Sprint 3",
+          notes: "notes",
+          completed: false,
+          project_id: 3
+        }
+      ]);
+    });
+};
